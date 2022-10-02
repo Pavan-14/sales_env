cea49b86fb832bf3010e2e3982a5312a3c699b0b
Pavan-14 <adapalapavan5@gmail.com>
2022-10-02 09:16:41 +0200

    added files and floders

---
+++ onlineshop/buynew/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class BuynewConfig(AppConfig):
+    name = 'buynew'
---
+++ onlineshop/buynew/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+    if request.method == "POST":
+        print('gotPOST')
+        form = request.POST
+        a = int(form.get("integer_A",None))
+        b = int(form.get("integer_B",None))
+        action = form.get("operation",None)
+        result = 0
+
+        if action == "add":
+            result = a+b
+        elif action == 'sub':
+            result = a-b
+        else:
+            print('select any one of the operator')
+        context = {"res":result}
+        print(context)
+    else:
+        print('gotGET')
+        context = {}
+
+    return render(request,"home.html",context)
